4264b7b45ff3691c62ca8f667cfc11721ee6cbab
xinghuoliaoyuan45 <18749818136@163.com>
2016-09-08 17:09:09 +0800

    master

---
+++ node_staticfs_use/09.js
@@ -0,0 +1,28 @@
+
+var express = require("express");
+var fs = require("fs");
+
+var app = express();
+
+app.use(haha);
+
+app.get("/admin",function(req,res){
+    res.send("管理员");
+})
+
+app.listen(3000);
+
+function haha(req,res,next){
+    var filePath = req.originalUrl;
+    //根据当前的网址，读取public文件夹中的文件
+    //如果有这个文件，那么渲染这个文件
+    //如果没有这个文件，那么next();
+    fs.readFile("./public/" + filePath,function(err,data){
+        if(err){
+            //文件不存在
+            next();
+            return;
+        }
+        res.send(data.toString());
+    });
+}
